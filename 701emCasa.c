59a5021de0da64eacdfb1b8d2e5a22f0dc2ee090
joaoMarceloBitar <joaomarcelodu@gmail.com>
2026-02-08 22:56:31 -0300

    codigo feito em casa e em aula adicionados

--- 701emCasa.c
+++ 701emCasa.c
@@ -10,7 +10,7 @@ struct TreeNode {
 struct TreeNode* insertIntoBST(struct TreeNode* root, int val);
 
 int main(){
-    struct TreeNode *root = (struct TreeNode *                  )malloc(sizeof(struct TreeNode));
+    struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
     struct TreeNode *l1 = (struct TreeNode *)malloc(sizeof(struct TreeNode));
     struct TreeNode *l2 = (struct TreeNode *)malloc(sizeof(struct TreeNode));
     struct TreeNode *l3 = (struct TreeNode *)malloc(sizeof(struct TreeNode));
@@ -25,18 +25,26 @@ int main(){
     l1->right=l3;
 
     l2->val=1;
-    l2->val=3;
+    l2->left=NULL;
+    l2->right=NULL;
+    
+    l3->val=3;
+    l3->left=NULL;
+    l3->right=NULL;
+
     r1->val=7;
+    r1->left=NULL;
+    r1->right=NULL;
 
     int val=5;
 
 
     insertIntoBST(root,val);
+    freeTree(root);
 }
 
 struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
     if(root!=NULL){
-        if(root->val){
             if(root->val > val){
                 if(root->left == NULL){
                 struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
@@ -47,7 +55,6 @@ struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
                 return root;
             }
             insertIntoBST(root->left,val);
-        }
         if(root->val < val){
             if(root->right==NULL){
                 struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
@@ -72,3 +79,22 @@ struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
 
     return root;
 }
+
+void freeTree(struct TreeNode* root) {
+    if (root == NULL) return;
+    freeTree(root->left);
+    freeTree(root->right);
+    free(root);
+}
+
+//Principais diferenças:
+
+//      correção do erro principal: não inicialização
+//      do nó no caso em que acha nulo na recursão
+
+//      validação booleana sem sentido " if ( root->val ) "
+//      q causava comportamento imprevisível
+
+
+//vazamento de memória:
+//      por nao mallocar sem dar free
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JOÃO MARCELO BITAR DUARTE
-# turma: M1
-# AED-LeetCode-701
+turma: M1
+AED-LeetCode-701
 
 Explicação do problema:
 
@@ -9,3 +9,20 @@ Explicação do problema:
     You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
 
     Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+
+# sumário do vídeo
+- apresentação
+    nome;
+    turma;
+    feito em aula;
+    explicação do problema;
+
+- código
+    mostrar no LeetCode;
+    explicar;
+    mostrar no vscode;
+    explicar dificuldades e diferenças;
+    mostrar submmit;
+    valgrind;
+    complexidade;
